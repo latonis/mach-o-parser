7b3455d2f993ae12a2bcce29c165288a30a1351f
Jacob Latonis <jlatonis@me.com>
2023-10-14 20:16:50 -0500

    dylibs

--- src/main.c
+++ src/main.c
@@ -30,24 +30,28 @@ struct macho_file {
     uint8_t should_swap;
     void*   header;
     void**  load_commands;
+    uint32_t n_dylibs;
+    struct dylib**  dylibs;
+    uint32_t n_rpaths;
+    // struct rpath**
 };
 
 int determine_type(struct macho_file *macho) {
-    struct header* temp_header = (struct header*) macho->header;
+    struct header* header = (struct header*) macho->header;
     macho->is_64bit = 0;
     macho->should_swap = 0;
     macho->is_universal = 0;
 
-    if (temp_header->magic == MH_CIGAM) {
+    if (header->magic == MH_CIGAM) {
         macho->should_swap = 1;
-    } else if (temp_header->magic == MH_MAGIC_64) {
+    } else if (header->magic == MH_MAGIC_64) {
         macho->is_64bit = 1;
-    } else if (temp_header->magic == MH_CIGAM_64) {
+    } else if (header->magic == MH_CIGAM_64) {
         macho->is_64bit = 1;
         macho->should_swap = 1;
-    } else if (temp_header->magic == FAT_MAGIC) {
+    } else if (header->magic == FAT_MAGIC) {
         macho->is_universal = 1;
-    } else if (temp_header->magic == FAT_CIGAM) {
+    } else if (header->magic == FAT_CIGAM) {
         macho->is_universal = 1;
         macho->should_swap = 1;
     }
@@ -56,41 +60,65 @@ int determine_type(struct macho_file *macho) {
 }
 
 uint8_t* get_header(uint8_t* input, struct macho_file *macho) {
-    macho->header = (struct header_64*) input;
-    return (uint8_t*) input + sizeof(struct header_64);
+    macho->header = (struct header*) input;
+    determine_type(macho);
+
+    if (macho->is_64bit) {
+        return (uint8_t*) input + sizeof(struct header_64);
+    } else {
+        return (uint8_t*) input + sizeof(struct header);
+    }
 }
 
 
 uint8_t* get_load_commands(uint8_t* input, struct macho_file* macho) {
-    
-    for (int i = 0; i < macho->header->n_commands; i++) {
+    struct header* header = (struct header*) macho->header;
+    macho->n_dylibs = 0;
+    macho->n_rpaths = 0;
+
+    for (int i = 0; i < header->n_commands; i++) {
         macho->load_commands[i] = (struct load_command*) input;
-        input = (uint8_t*) (input + ((struct load_command*) macho->load_commands[i])->cmdsize);
+        struct load_command* lc = macho->load_commands[i];
+        switch (lc->cmd) {
+            case LC_RPATH:
+                macho->n_rpaths++;
+                break;
+            case LC_ID_DYLIB:
+            case LC_LOAD_DYLIB:
+            case LC_LOAD_WEAK_DYLIB:
+            case LC_REEXPORT_DYLIB:
+                macho->n_dylibs++;
+                break;
+            default:
+                break;
+        }
+        input = (uint8_t*) (input + (lc->cmdsize));
     }
 
     return input;
 }
 
 int print_stats(struct macho_file* macho) {
-    struct header* temp_header = (struct header*) macho->header;
-    if (macho->is_64bit) {
-        temp_header = 
-    }
-    printf("Magic: %x\n", macho->header->magic);
-    printf("CPU Type: %x\n", macho->header->cpu_type);
-    printf("CPU Subtype: %x\n", macho->header->cpu_subtype);
-    printf("# of Commands: %x\n", macho->header->n_commands);
-    printf("Size of Commands: %d\n", (int) macho->header->size_commands);
-    printf("Swap: %u\n64 bit: %u\nFat: %u\n", macho->should_swap, macho->is_64bit, macho->is_universal);
+    struct header* header = (struct header*) macho->header;
 
+    printf("Magic: %x\n", header->magic);
+    printf("CPU Type: %x\n", header->cpu_type);
+    printf("CPU Subtype: %x\n", header->cpu_subtype);
+    printf("# of Commands: %x\n", header->n_commands);
+    printf("Size of Commands: %d\n", (int) header->size_commands);
+    printf("Swap: %u\n64 bit: %u\nFat: %u\n", macho->should_swap, macho->is_64bit, macho->is_universal);
     return 0;
 }
 
 int print_load_commands(struct macho_file* macho) {
+    struct header* header = (struct header*) macho->header;
 
-    for (int i = 0; i < macho->header->n_commands; i++) {
+    for (int i = 0; i < header->n_commands; i++) {
         printf("cmd: %x\ncmdsize: %x\n", ((struct load_command*) macho->load_commands[i])->cmd, ((struct load_command*) macho->load_commands[i])->cmdsize); 
     }
+
+    printf("Number of Dylibs: %u", (int) macho->n_dylibs);
+
     return 0;
 }
 
@@ -132,7 +160,9 @@ int main(int argc, char **argv) {
     uint8_t* input = read_file(input, fp);
 
     struct macho_file* macho = malloc(sizeof(struct macho_file));
-    macho->load_commands = malloc(sizeof(struct command *) * macho->header->n_commands);
+    struct header* header = (struct header*) macho->header;
+
+    macho->load_commands = malloc(sizeof(struct command *) * header->n_commands);
 
     input = get_header(input, macho);
     determine_type(macho);
